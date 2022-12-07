60012556d313e21764cf44a1d7535a0911e88268
Dorinel <gimiri99@gmail.com>
2022-12-06 19:07:31 -0500

    added comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,38 +5,54 @@ import java.awt.event.ActionListener;
 //Dorin Tihon
 
 /**
- *
+ The class TicTacToe which creates a GUI interface that implements ActionListener.
+ It has all the methods that are necessary for making the game working.
+ It holds the current player, and it creates the JFrame where the 2D array of buttons is
+ implemented.
+ It has the main method where class is initialized
  */
 public class TicTacToe implements ActionListener {
+
+    //Variables
     char currentPlayer;
     JButton[][] buttons = new JButton[3][3];
     TttCell[][] cells = new TttCell[3][3];
     JFrame frame;
     JLabel textField;
 
-    /**
-     * @param e the event to be processed
-     * @author Alacataba Inc.
-     */
+
+    //actionPerformed() method that performs the action when the
+    //selected button is pressed.
     public void actionPerformed(ActionEvent e) {
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
+
+                //if method to get the button on which the mouse clicks on
                 if (e.getSource() == buttons[i][j]) {
+
+                    //if method to check if the selected cell is occupied
                     if (!cells[i][j].isOccupied()) {
+
+                        //if method to perform action if the current player is X
                         if (currentPlayer == 'X') {
                             cells[i][j].setPlayer(currentPlayer);
                             buttons[i][j].setText(String.valueOf(cells[i][j].getPlayer()));
                             buttons[i][j].setForeground(new Color(175, 75, 75));
                             cells[i][j].setOccupied(true);
                             displayWinner();
-                        } else {
+                        }
+                        //else method to perform action if the current player is O
+                        else {
                             cells[i][j].setPlayer(currentPlayer);
                             buttons[i][j].setText(String.valueOf(cells[i][j].getPlayer()));
                             buttons[i][j].setForeground(new Color(54, 87, 44));
                             cells[i][j].setOccupied(true);
                             displayWinner();
                         }
-                    } else {
+                    }
+                    //else method that displays a window if the selected cell is occupied
+                    else {
                         JOptionPane winner = new JOptionPane();
                         JOptionPane.showMessageDialog(winner, "This cell is occupied", "Occupied", JOptionPane.ERROR_MESSAGE);
                     }
@@ -45,18 +61,22 @@ public class TicTacToe implements ActionListener {
         }
     }
 
-    public void initialize() {
+    //startGame() method that creates the JFrame and formats that frame
+    //in the proper way.
+    public void startGame() {
         currentPlayer = 'X';
         frame = new JFrame("Tic Tac si Toe");
+
+        //JLabel object that shows at the top of the frame the current player
         textField = new JLabel();
-        JPanel buttonGrid = new JPanel();
         textField.setHorizontalAlignment(JLabel.CENTER);
         textField.setText("Current player: " + currentPlayer);
         textField.setForeground(new Color(24, 38, 89));
         textField.setFont(new Font("Arial", Font.BOLD, 50));
         frame.add(textField, BorderLayout.NORTH);
 
-
+        //JPanel object that define the buttons
+        JPanel buttonGrid = new JPanel();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 buttons[i][j] = new JButton();
@@ -70,12 +90,16 @@ public class TicTacToe implements ActionListener {
 
         // setting grid layout of 3 rows and 3 columns
         buttonGrid.setLayout(new GridLayout(3, 3));
+
+
         frame.add(buttonGrid);
         frame.setSize(550, 550);
         frame.setVisible(true);
     }
 
-    public boolean rowWin() {
+
+    //checkRow() method that checks if there are 3 X's or 3 O's in a row
+    public boolean checkRow() {
         int i = 0;
         for (int j = 0; j < 3; j++) {
             if (buttons[i][0].getText().equals(buttons[i][1].getText()) &&
@@ -88,7 +112,8 @@ public class TicTacToe implements ActionListener {
         return false;
     }
 
-    public boolean columnWin() {
+    //checkColumn() method that checks if there are 3 X's or 3 O's in a column
+    public boolean checkColumn() {
         int i = 0;
         int x = 0;
         for (int j = 0; j < 3; j++) {
@@ -102,7 +127,8 @@ public class TicTacToe implements ActionListener {
         return false;
     }
 
-    public boolean diagonalWin() {
+    //checkDiagonal() method that checks if there are 3 X's or 3 O's in a diagonal
+    public boolean checkDiagonal() {
         if (buttons[0][0].getText().equals(buttons[1][1].getText()) &&
                 buttons[0][0].getText().equals(buttons[2][2].getText()) &&
                 !buttons[0][0].getText().equals("")) {
@@ -113,26 +139,26 @@ public class TicTacToe implements ActionListener {
                 !buttons[0][2].getText().equals("");
     }
 
+    //checkWinner() method that check if there is a winning combination
+    public boolean checkWinner() {
+        return checkRow() || checkColumn() || checkDiagonal();
+    }
+
+    //checkDraw() method that check if there are no empty cells
     public boolean checkDraw() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (buttons[i][j].getText().equals("")) {
+                if (!cells[i][j].isOccupied()) {
                     return false;
                 }
             }
         }
         return true;
+
     }
 
-    public boolean checkWinner() {
-        return rowWin() || columnWin() || diagonalWin();
-    }
 
-    public void resetGame() {
-        frame.dispose();
-        initialize();
-    }
-
+    //displayWinner() method that create a window with the winner and option to play again
     public void displayWinner() {
         if (checkWinner()) {
             JOptionPane winner = new JOptionPane();
@@ -142,7 +168,7 @@ public class TicTacToe implements ActionListener {
             } else System.exit(0);
         } else if (checkDraw()) {
             JOptionPane winner = new JOptionPane();
-            int result = JOptionPane.showConfirmDialog(winner, "Draw", "Game over", JOptionPane.YES_NO_OPTION);
+            int result = JOptionPane.showConfirmDialog(winner, "Draw. Play again?", "Game over", JOptionPane.YES_NO_OPTION);
             if (result == JOptionPane.YES_OPTION) {
                 resetGame();
             } else System.exit(0);
@@ -152,9 +178,15 @@ public class TicTacToe implements ActionListener {
         }
     }
 
+    //resetGame() method that resets the game
+    public void resetGame() {
+        frame.dispose();
+        startGame();
+    }
 
+    //main method
     public static void main(String[] args) {
         TicTacToe t = new TicTacToe();
-        t.initialize();
+        t.startGame();
     }
 }
--- src/TttCell.java
+++ src/TttCell.java
@@ -1,39 +1,54 @@
+//Dorin Tihon
+
+/**
+ TttCell class to define the cell object that holds the player and if the cell is occupied
+ */
 public class TttCell {
+
+    //Variables
     boolean occupied;
     char player;
 
+    //Default constructor
     public TttCell() {
         occupied = false;
         player = ' ';
     }
 
+    //Parameter constructor
     public TttCell(boolean occupied, char player) {
         this.occupied = occupied;
         this.player = player;
     }
 
+    //isOccupied() method that returns the occupied variable
     public boolean isOccupied() {
         return occupied;
     }
 
+    //setOccupied() method that sets the occupied variable to the given parameter
     public void setOccupied(boolean occupied) {
         this.occupied = occupied;
     }
 
+    //getPlayer() method that returns the player variable
     public char getPlayer() {
         return player;
     }
 
+    //setPlayer() method that sets the player variable to the given parameter
     public void setPlayer(char player) {
         this.player = player;
     }
 
+    //toString method to print the TttCell object information
     @Override
     public String toString() {
         return "This cell is occupied:" + occupied +
                 ", by player:" + player;
     }
 
+    //equals method
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
